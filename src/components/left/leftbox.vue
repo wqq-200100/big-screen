320d529eaad0aee70064a704528b9250ee9e1160
包谷清清 <776570787@qq.com>
2022-11-14 16:45:21 +0800

    修改数据

---
+++ src/components/left/leftbox.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <boxTitle v-slot:headerleft> 政务云资源总概览 </boxTitle>
+
+    <!-- 上云概览 -->
+      <overview/>
+
+    <!-- 云上资源利用率 -->
+    <utilization/>
+
+    <!-- 上云系统数TOP10 -->
+    <system/>
+
+    <!-- 安全态势 -->
+    <safe/>
+  </div>
+</template>
+<script setup>
+import boxTitle from "../../global/boxTitle.vue";
+import overview from "./overview/overview.vue";
+import utilization from "./utilization/utilization.vue";
+import system from "./system/system.vue";
+import safe from "./safe/safe.vue";
+
+</script>
+
+
+  
